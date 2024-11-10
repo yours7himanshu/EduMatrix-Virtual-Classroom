60f05e013905a1fed1fafa829ecfaa80f70e7bcc
Guni paliwal <gpaliwal2605@gmail.com>
2024-11-10 14:52:56 +0530

    Signup layout

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,34 @@
-
+import { useState } from "react";
 
 function SignUp() {
+  const [state, setState] = useState("Signup");
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      
-    </div>
-  )
+    <form className="min-h-[80vh] flex justify-center items-center">
+      <div className="">
+        <p>{state == "Signup" ? "Create Account" : "Login Account"}</p>
+        <div>
+          <p>Full Name</p>
+          <input type="text" />
+        </div>
+        <div>
+          <p>Username</p>
+          <input type="text" />
+        </div>
+        <div>
+          <p>Email</p>
+          <input type="email" />
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" />
+        </div>
+      </div>
+    </form>
+  );
 }
 
-export default SignUp
+export default SignUp;
