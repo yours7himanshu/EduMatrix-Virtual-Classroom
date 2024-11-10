5b0b010c12df0f2b4063fc7d64bd4623a7f5bc4c
harshita narang <harshitanarang30@gmail.com>
2024-11-10 16:56:08 +0530

    created login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
 
     </Routes>
