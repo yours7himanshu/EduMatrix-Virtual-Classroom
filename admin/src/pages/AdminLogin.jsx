1cf1a9a7a218d19d4ef52787b11b9d05a7f663b5
yours7himanshu <dinkarhimanshu78@gmail.com>
2024-11-13 00:57:07 +0530

    trying to fix production related bug

--- admin/src/pages/AdminLogin.jsx
+++ admin/src/pages/AdminLogin.jsx
@@ -2,7 +2,7 @@ import { useState  } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import {axios} from 'axios';
+import axios from 'axios';
 
 function AdminLogin() {
     const [email,setEmail]=useState('');
