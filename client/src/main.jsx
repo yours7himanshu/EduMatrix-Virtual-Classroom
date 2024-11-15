6424f8cd56c5ceebc4b02d77966f228412fb4573
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2024-11-16 01:36:38 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from './context/AuthContext'; // Ensure correct import path
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <AuthContextProvider>  {/* Wrap App inside AuthContextProvider */}
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
