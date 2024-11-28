5ed57d8d612a0dda305cda24f95f578ae207153f
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2024-11-28 12:54:43 +0530

    Student enrollement feature with its image uplodation is completed successfully

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {enrollStudent,studentDetail} = require('../controllers/studentController');
+const {enrollStudent} = require('../controllers/studentController');
 const studentRouter = express.Router();
+const upload = require('../middlewares/multer');
 
-studentRouter.post("/enroll-student",enrollStudent);
-studentRouter.get('/student-detail',studentDetail);
+studentRouter.post("/enroll-student", upload.single("avatar"), enrollStudent);
+// studentRouter.get('/student-detail',studentDetail);
 
 module.exports=studentRouter;
