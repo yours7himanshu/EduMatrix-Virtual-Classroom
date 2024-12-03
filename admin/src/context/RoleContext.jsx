864b1200fad8cf4262d187e153b572e6785df3f7
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2024-12-03 17:16:14 +0530

    fixed updation of the slidebar component

--- admin/src/context/RoleContext.jsx
+++ admin/src/context/RoleContext.jsx
@@ -1,23 +1,21 @@
-import React, { createContext, useState, useEffect } from 'react';
-import {jwtDecode} from 'jwt-decode';
-
+import React, { createContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const RoleContext = createContext();
-
 
 const RoleProvider = ({ children }) => {
     const [userRole, setUserRole] = useState(null);
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         if (token) {
             const decodedToken = jwtDecode(token);
-            setUserRole(decodedToken.role);
+            setUserRole(decodedToken.role); 
         }
-    }, []);
+    }, []); // Runs only on initial mount, updates based on token
 
     return (
-        <RoleContext.Provider value={userRole}>
+        <RoleContext.Provider value={{ userRole, setUserRole }}>
             {children}
         </RoleContext.Provider>
     );
--- admin/src/layout/AppLayout.jsx
+++ admin/src/layout/AppLayout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Sidebar from '../shared/Sidebar'
+import { RoleProvider } from "../context/RoleContext";
+
 
 const AppLayout = () => (WrapLayoutComponent)=> {
     return (props)=>{
         return (
             <div  className='flex  h-screen w-screen' >
+              <RoleProvider>
+
               <Sidebar/>
+              </RoleProvider>
               <div className='w-full h-screen overflow-x-hidden' >
 
               <WrapLayoutComponent {...props} />
