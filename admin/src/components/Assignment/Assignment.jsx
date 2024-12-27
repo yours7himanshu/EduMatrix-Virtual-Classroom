10f7e074596a6e6a25f4fbce9a40ab22c67efe11
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2024-12-27 12:07:10 +0530

    added license

--- admin/src/components/Assignment/Assignment.jsx
+++ admin/src/components/Assignment/Assignment.jsx
@@ -1,6 +1,21 @@
 // Copyright (c) 2024 Himanshu Dinkar
-// Licensed under the MIT Lincense. See LICENSE file in the project directory for full License information
+//  See LICENSE file in the project directory for full License information
+/*
 
+Copyright 2024 Himanshu Dinkar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import { useState } from "react";
 import { toast } from "react-toastify";
