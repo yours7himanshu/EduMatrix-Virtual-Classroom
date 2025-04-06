95fdb4a6be20807982b724f2eb423022602fb334
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2025-04-06 19:15:20 +0530

    commented out some unused code

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -21,19 +21,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ContextStoreProvider } from "./store/ContextStore.jsx";
+
 import AuthContextProvider from "./context/AuthContext.jsx"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-
+ 
     <BrowserRouter>
-      <ContextStoreProvider>
-       
+      <AuthContextProvider>
         <App />
-
-      </ContextStoreProvider>
+      </AuthContextProvider>
     </BrowserRouter>
-    </AuthContextProvider>
+   
   </StrictMode>
 );
