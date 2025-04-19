06c28c595d3d09611d16b85b67749c0758c71bf3
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2025-04-19 23:32:25 +0530

    updated python script backend logic

--- server/controllers/notesUploadController.js
+++ server/controllers/notesUploadController.js
@@ -1,10 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
-
+const {spawn} = require('child_process')
 const UploadPdfFile = async(req,res)=>{
 
 let pdfUrl;
-
+let output = "";
+let errorOutput = "";
 if(!req.file){
     return res.status(404).json({
         success: false,
@@ -24,26 +25,54 @@ try {
       );
       stream.end(req.file.buffer); 
     });
-
+    // for python script reading
     pdfUrl = result.secure_url;
-    
-    // Return successful response with the PDF URL
-    return res.status(200).json({
-      success: true,
-      message: "PDF file uploaded successfully",
-      pdfUrl: pdfUrl
-    });
-    
-  } catch (uploadError) {
-    console.error("Error uploading avatar to Cloudinary:", uploadError);
-    return res.status(500).json({
-      success: false,
-      message: "Error uploading pdf file",
-      error: uploadError.message,
-    });
-  }
 
+
+    const pythonProcess = spawn('python', ['python_rec/text_summarization.py', pdfUrl]);
+    pythonProcess.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+    pythonProcess.stderr.on('data', (data) => {
+      errorOutput += data.toString();
+    });
+    pythonProcess.on('close', (code) => {
+      if(errorOutput){
+        return res.status(500).json({
+          success: false,
+          message: "Error processing pdf file",
+          
+        })
+      }
+      
+      if(code !==0){
+        return res.status(404).json({
+          success:false,
+          message:"No output recieved"
+        })
+      }
+      else {
+      
+          const result = JSON.parse(output);
+          return res.status(200).json({
+            success:true,
+            result:result
+            
+            
+        })
+
+            
+            
+     
+     }
+    });
+    // Return successful response with the PDF URL
+  }catch(error){
+return res.status(500).json ({
+  success:false,
+  message:"some error occured"
+})
 
 }
-
+}
 module.exports={UploadPdfFile}
