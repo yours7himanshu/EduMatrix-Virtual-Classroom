d71f0220e9bd679f9704acf9d2f8ca26f9c97882
Ayush743 <ayush210042@gmail.com>
2025-04-22 21:54:10 +0530

    merging dataframes and implementing dynamic dashboard

--- python_rec/student_analysis.py
+++ python_rec/student_analysis.py
@@ -10,17 +10,22 @@ from dotenv import load_dotenv
 import seaborn as sns
 import os
 
-load_dotenv(dotenv_path='.env')
+load_dotenv(dotenv_path='EduMatrix-Virtual-Classroom/python_rec/.env')
 
 mongo_URI = os.getenv("MONGO_URI")
 client = MongoClient(f"{mongo_URI}")
 db = client["test"]
-collection = db["demo"]
-cursor = collection.find()
-df = pd.DataFrame(list(cursor))
-branches=df.groupby("Branch")['Attendance (%)'].mean().reset_index()['Branch']
-att_mean=df.groupby("Branch")['Attendance (%)'].mean().reset_index()['Attendance (%)']
-marks_mean=df.groupby("Branch")['Marks (%)'].mean().reset_index()['Marks (%)']
+collection1 = db["registrarfees"]
+collection2 = db["studentmarksattendances"]
+cursor1 = collection1.find()
+cursor2 = collection2.find()
+registrar = pd.DataFrame(list(cursor1))
+teacher= pd.DataFrame(list(cursor2))
+df=registrar.merge(teacher,on='RollNumber')
+
+branches=df.groupby("Branch")['Attendance'].mean().reset_index()['Branch']
+att_mean=df.groupby("Branch")['Attendance'].mean().reset_index()['Attendance']
+marks_mean=df.groupby("Branch")['Marks'].mean().reset_index()['Marks']
 
 def create_bar_plot(x, y,title,xlabel):
     buf = io.BytesIO()
@@ -47,9 +52,9 @@ def create_bar_plot(x, y,title,xlabel):
 
 def top_students():
     buf = io.BytesIO()
-    x=df.loc[df.groupby('Branch')['Marks (%)'].idxmax()][['Branch', 'Marks (%)', 'Name']]
-    name=x['Name']
-    marks=x['Marks (%)']
+    x=df.loc[df.groupby('Branch')['Marks'].idxmax()][['Branch', 'Marks', 'Name_x']]
+    name=x['Name_x']
+    marks=x['Marks']
     branch=x['Branch']
     plt.figure(figsize=(12,5))
     plot=sns.barplot(data=df, x=name, y=marks, hue=branch)
@@ -81,12 +86,12 @@ def pieplot():
 
 def scatter():
     buf = io.BytesIO()
-    new_df=df[['Attendance (%)','Marks (%)','Branch']]
+    new_df=df[['Attendance','Marks','Branch']]
     plt.figure(figsize=(8, 6))
-    sns.scatterplot(data=new_df, x=df['Attendance (%)'], y=new_df['Marks (%)'], hue=new_df['Branch'], s=100)
+    sns.scatterplot(data=new_df, x=df['Attendance'], y=new_df['Marks'], hue=new_df['Branch'], s=100)
     plt.title("Attendance vs Marks")
-    plt.xlabel("Attendance (%)")
-    plt.ylabel("Marks (%)")
+    plt.xlabel("Attendance")
+    plt.ylabel("Marks")
     plt.grid(True)
     plt.tight_layout()
     plt.savefig(buf, format='png')
@@ -99,9 +104,9 @@ def scatter():
 def fees_status():
     
     buf = io.BytesIO()
-    grouped = df.groupby(['Branch', 'fees_status']).size().reset_index(name='count')
+    grouped = df.groupby(['Branch', 'Fees_status']).size().reset_index(name='count')
     plt.figure(figsize=(10, 6))
-    sns.barplot(data=grouped, x='Branch', y='count', hue='fees_status', palette='Set2')
+    sns.barplot(data=grouped, x='Branch', y='count', hue='Fees_status', palette='Set2')
     plt.title('Fee Status by Branch')
     plt.xlabel('Branch')
     plt.ylabel('Number of Students')
